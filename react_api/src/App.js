525ee029ad2c4931ca9ed07a822a88e6250d7598
Rajeshwari <rajeshwarirajeshwari34879@gmail.com>
2026-03-25 11:37:21 +0530

    adding the react new project

---
+++ react_api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import UserDetails from './UserDetails';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-center'>Product Details</h1>
+     <UserDetails />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_api/src/UserDetails.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./UserDetails.css";
+import './App.css'
+
+const UserDetails = ()=> {
+
+    const [user, setUser] = useState([]);
+
+    const UserDetails = "https://fakestoreapi.com/products"
+
+    const UserHandler = async() => {
+        const respond = await fetch(UserDetails)
+        const newData = await respond.json();
+        setUser(newData)
+
+    }
+    useEffect(()=> {
+        
+        console.log(UserHandler())
+       
+    },[])
+    
+    console.log(user)
+
+    return(
+        <div className="wrapper">
+            { user.map((item)=>{
+                return(
+                    <div className="card">
+                        <img src={item.image} alt="" />
+                        <div className="card-body">
+                            <h3><b>Title</b> : {item.title}</h3>
+                            <p><b>Category</b> : {item.category}</p>
+                            <p><b>Price</b>: {item.price}</p>
+                            <p><b>Rating</b>: {item.rating.rate}</p>
+                            <p>{item.description}</p>
+                        </div>
+                    </div>
+                    )
+                }
+                )
+            }
+        </div> 
+    )
+}
+
+export default UserDetails
